107cfb9c8a0f200f57e1519defa2e0d4d7db316c
Wallen <872465234@qq.com>
2020-08-12 11:34:43 +0800

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spring-annotation-driven-development
 Spring注解驱动开发学习笔记
+
+目录结构
+```
+com.spring.annotation.wallen---钱文龙文件目录
+```
---
+++ src/main/java/com/spring/annotation/wallen/Initialization.java
@@ -0,0 +1,10 @@
+package com.spring.annotation.wallen;
+
+/**
+ * 初始化项目，无实际意义
+ *
+ * @author Wallen
+ * 2020/8/12 11:32
+ */
+public class Initialization {
+}
